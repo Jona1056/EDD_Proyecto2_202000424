9ff1f4bacf01fbcc6e101796dc3fc3e846dfea4a
Jona1056 <3685713940101@ingenieria.usac.edu.gt>
2022-12-21 00:37:52 -0600

    lista clientes

--- jss/main.js
+++ jss/main.js
@@ -74,10 +74,10 @@ function Back(){
     login_us(user, passw) {
       if (!this.size) {
         swal("Error", "NO HAY USUARIOS", "error");
+        return true;
       } else {
         let recorrido = this.head;
         while (recorrido) {
-          if (!recorrido.admin) {
             console.log(recorrido.username);
             if (user == recorrido.username && passw == recorrido.password) {
               document.getElementById("LOGIN-1").style.display = "none";
@@ -93,85 +93,55 @@ function Back(){
               swal("Error", "USUARIO NO ENCONTRADO", "error");
               return "error";
             }
-          } else {
-            if (!recorrido) {
-              swal("Error", "USUARIO NO ENCONTRADO", "error");
-              return "error";
-            }
-            recorrido = recorrido.next;
-          }
+         
         }
       }
+   
     }
-    show() {
-      if (!this.size) {
-        return "Hola";
-      } else {
-        let recorrido = this.head;
-        while (recorrido) {
-          console.log(recorrido.username);
-        }
-      }
-    }
+  
     graph(idDiv) {
-      // creamos la variable del diagraph
+
       let graphviz =
-        'digraph SimpleList{\nnode[shape= box, fillcolor="#FFFFFF", style= filled];\nbgcolor = "#CD1CED ";\nranksep = 0.5;\nnodesep = 0.5;\nsubgraph cluster_A{\nlabel = "Clientes";\nbgcolor = "#BC70FC";\nfontcolor ="#3A0964";\nfontsize = 30;\n\n ';
-  
-  
+        'digraph SimpleList{\nnode[shape= box, fillcolor="#FFFFFF", style= filled];\nbgcolor = "  #00ccff ";\nranksep = 0.5;\nnodesep = 0.5;\nsubgraph cluster_A{\nlabel = "Clientes";\nbgcolor = "  #00ffad ";\nfontcolor ="black ";\nfontsize = 30;\n\n ';
       let current = this.head;
       let i = 1;
-  
       while (current != null) {
-        // recorremos la lista hasta que sea null y agregamos un indicie cliente1
         graphviz += "cliente" + i + '[label="' + current.name + '"];\n';
-  
         i++;
         current = current.next;
       }
-  
       graphviz += "\n";
   
-      //we point the customer nodes
-  
+
       current = this.head;
       i = 1;
-  
       while (current != null) {
         // aqui de igual manera hasta que sea null y agragemos el indice
         if (current.next != null) {
           graphviz += "cliente" + i + " -> cliente" + (i + 1) + "\n";
         }
-  
         i++;
         current = current.next;
       }
   
       graphviz += "\n";
   
-      //we aling the nodes
+
   
       current = this.head.next;
       i = 1;
-  
       graphviz += "{rank = same; cliente" + i;
-  
       i++;
-  
       while (current != null) {
         // en esta parte agreagamos el valor de cliente con la posicion i
         graphviz += "; cliente" + i;
-  
         i++;
         current = current.next;
       }
   
       graphviz += "};\n\n}\n}";
-  
       console.log(graphviz);
-  
       let id = "#" + idDiv;
-  
       d3.select(id) //creamos con d3 el rendeDot y el paragrah
         .graphviz()
   
@@ -198,7 +168,7 @@ const admin = {
     correo:"ingenieira@usac.com",
     telefono: 12345678,
   };
-const clientes = new Cliente();
+let clientes = new listaenlazada();
 
   //--------------------------------------------------------
 
@@ -220,6 +190,7 @@ function loginadmin(){
                 swal("Oops!", "ADMINISTRADOR ERRONEO", "error");
             }
         }else{
+          
             clientes.login_us(user,password);
 
 
@@ -232,4 +203,42 @@ function loginadmin(){
 
 
 //---------------------------------
+// FUNCIONAMIENTO CLIENTES
 
+//cargar CLIENTES
+function cargar_clientes(e) {
+  var archivo = e.target.files[0];
+  document.getElementById("fichero-usuarios").files[0];
+
+  if (!archivo) {
+    return;
+  }
+  let lector = new FileReader();
+  lector.onload = function (e) {
+    let contenido = e.target.result;
+
+    const _clients = JSON.parse(contenido);
+
+    for (const i in _clients) {
+      let client1 = _clients[i];
+      clientes.add(
+        client1.dpi,
+        client1.nombre_completo,
+        client1.nombre_usuario,
+        client1.correo,
+        client1.contrasenia,
+        client1.telefono
+      );
+    }
+  };
+  lector.readAsText(archivo);
+}
+document
+  .getElementById("fichero-usuarios")
+  .addEventListener("change", cargar_clientes, false);
+
+//GRAFO CLIENTES
+
+function grafoclientes(){
+  clientes.graph("showlist");
+}
